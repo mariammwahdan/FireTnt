5777d12686535235d05bf433a3f6bde92ea9e8cc
Mariam Wahdan <110642313+mariammwahdan@users.noreply.github.com>
2025-04-22 22:08:36 +0200

    added property microservice with redis caching

--- Properties/src/main/java/com/example/Properties/Property/DTO/CreatePropertyDTO.java
+++ Properties/src/main/java/com/example/Properties/Property/DTO/CreatePropertyDTO.java
@@ -1,4 +1,61 @@
 package com.example.Properties.Property.DTO;
 
+import jakarta.validation.constraints.*;
+
+
 public class CreatePropertyDTO {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @Positive(message = "Price per night must be a positive number")
+    private double pricePerNight;
+
+    @NotNull(message = "Is booked status is required")
+    private boolean isBooked;
+
+    @Positive(message = "Host ID must be a positive number")
+    private Long hostId; // Foreign key reference to User Microservice
+    // Getters and Setters
+
+
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+    public void setBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
+    public Long getHostId() {
+        return hostId;
+    }
+    public void setHostId(Long hostId) {
+        this.hostId = hostId;
+    }
+
+
+
+
 }
--- Properties/src/main/java/com/example/Properties/Property/Property.java
+++ Properties/src/main/java/com/example/Properties/Property/Property.java
@@ -1,4 +1,66 @@
 package com.example.Properties.Property;
-
+import jakarta.persistence.*;
+@Entity
+@Table(name = "properties")
 public class Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer propertyId;
+
+    private String title;
+    private String description;
+    private double pricePerNight;
+    private boolean isBooked;
+
+    private Long hostId; // Foreign key reference to User Microservice
+
+
+    public Integer getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(Integer propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
+
+    public Long getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(Long hostId) {
+        this.hostId = hostId;
+    }
 }
