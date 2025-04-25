775bc2024f300fe2487fc87dc8139b012f4ea459
Mariam Wahdan <110642313+mariammwahdan@users.noreply.github.com>
2025-04-26 01:26:50 +0300

    fixing errors

--- UserAuthenticationAndRoleManagement/src/main/java/com/example/UserAuthenticationAndRoleManagement/Guest/GuestRepository.java
+++ UserAuthenticationAndRoleManagement/src/main/java/com/example/UserAuthenticationAndRoleManagement/Guest/GuestRepository.java
@@ -1,9 +1,4 @@
 package com.example.UserAuthenticationAndRoleManagement.Guest;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuestRepository extends JpaRepository<Guest, Long> {
-    boolean existsByEmail(String email);
+public interface GuestRepository {
 }
