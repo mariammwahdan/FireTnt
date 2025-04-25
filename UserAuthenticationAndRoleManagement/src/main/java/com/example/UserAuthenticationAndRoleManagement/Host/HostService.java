d63df6f1f46485d27a52f03a8483214534dbaf5a
Mariam Wahdan <110642313+mariammwahdan@users.noreply.github.com>
2025-04-25 22:29:01 +0300

    added host services in user serices to try to communicate with properties --intial trial

--- UserAuthenticationAndRoleManagement/src/main/java/com/example/UserAuthenticationAndRoleManagement/Host/HostService.java
+++ UserAuthenticationAndRoleManagement/src/main/java/com/example/UserAuthenticationAndRoleManagement/Host/HostService.java
@@ -1,4 +1,38 @@
 package com.example.UserAuthenticationAndRoleManagement.Host;
 
+
+import com.example.UserAuthenticationAndRoleManagement.Config.RestTemplateConfig;
+import com.example.UserAuthenticationAndRoleManagement.Host.Client.PropertyClient;
+import com.example.UserAuthenticationAndRoleManagement.Host.DTO.CreatePropertyDTO;
+import com.example.UserAuthenticationAndRoleManagement.Host.DTO.PropertyDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class HostService {
+    private final RestTemplate restTemplate;
+    private final PropertyClient propertyClient;
+    private static final String PROPERTY_SERVICE_URL = "http://localhost:8082/api/properties";
+
+
+    @Autowired
+    public HostService(PropertyClient propertyClient, RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+        this.propertyClient = propertyClient;
+    }
+    public List<PropertyDTO> getPropertiesForHost(Long hostId) {
+        String url = PROPERTY_SERVICE_URL + "/host/" + hostId;
+        PropertyDTO[] response = restTemplate.getForObject(url, PropertyDTO[].class);
+        return Arrays.asList(response);
+    }
+
+    public void createProperty(CreatePropertyDTO dto) {
+        String url = PROPERTY_SERVICE_URL + "/create";
+        restTemplate.postForObject(url, dto, Void.class);
+    }
+
 }
